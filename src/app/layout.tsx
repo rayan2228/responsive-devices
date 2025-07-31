fcaea391dbacfa2ed2ae9af0fe130b97fc47841b
rayan2228 <rayanh.dev@gmail.com>
2025-07-31 14:33:12 +0600

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Head from 'next/head';
+import Script from 'next/script';
 import "./globals.css";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -55,9 +55,11 @@ export const metadata: Metadata = {
   },
   verification: {
     google: 'pODC2wrngSGaNlkVCpYajNJvI748aMcdqLTKpxtRIpc',
-  }
+  },
+  alternates: {
+    canonical: 'https://www.responsihub.com/',
+  },
 };
-
 
 const structuredData = {
   "@context": "https://schema.org",
@@ -68,7 +70,7 @@ const structuredData = {
   applicationCategory: "DesignApplication",
   offers: {
     "@type": "Offer",
-    price: "0", // Or real price
+    price: "0",
     priceCurrency: "USD"
   },
   aggregateRating: {
@@ -85,24 +87,62 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MMZVGMSM"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MMZVGMSM');
+            `,
+          }}
+        />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3F2DSRTZ9P"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3F2DSRTZ9P');
+            `,
+          }}
+        />
+
+        {/* Structured data JSON-LD */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
-        <link rel="canonical" href="https://www.responsihub.com/" />
-      </Head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+
         {children}
+
         <SpeedInsights />
-        <GoogleAnalytics gaId="G-3F2DSRTZ9P" />
-        <GoogleTagManager gtmId="GTM-MMZVGMSM" />
       </body>
     </html>
   );
 }
-
-
-
