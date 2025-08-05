a32b0fa55c1e85f92fc1620cfc3e927decf34712
rayan2228 <rayanh.dev@gmail.com>
2025-08-05 18:27:43 +0600

    featL add loading.update layout.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+export default function Loading() {
+    return <div className="flex justify-center items-center h-screen">
+        <div className="rounded-full h-20 w-20 bg-blue-500 animate-ping" />
+    </div>
+
+}
